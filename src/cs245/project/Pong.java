d9355a350084250f385f19ecf0fc30bbd822b357
gary <50461559+aSilverSoul@users.noreply.github.com>
2019-10-11 15:16:28 -0700

    update header

--- src/cs245/project/Pong.java
+++ src/cs245/project/Pong.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/***************************************************************
+* file: Pong.java
+* author: Gary Machorro, Brandon Helt, Sonya Eller
+* class: CS 2450 – Programming Graphical User Interfaces
+*
+* assignment: Project 1-finalversion
+* date last modified: 10/11/19
+* purpose: The pong.java which contains pongengine, pong, and status bar 
+* classes. Has everthing for pong to run
+*
+****************************************************************/ 
+
+
 package cs245.project;
 import java.awt.BorderLayout;
 import java.awt.Color;
